f3bc98a20069dc14a8e6265feba94edea7645215
Awesome <yourarj@gmail.com>
2022-03-20 13:35:00 +0530

    feat(greeter): added types required

--- pallets/greeter/src/lib.rs
+++ pallets/greeter/src/lib.rs
@@ -2,11 +2,17 @@
 
 pub use pallet::*;
 
+/// ## Greeter Pallet
+/// this pallet lets user greet the
+/// blockchain
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
+	/// ### Greeter Pallet Struct
+	/// pallet struct of Greeter
+	/// business logic & callable gather around it
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
@@ -14,6 +20,41 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	// Membership enum
+	// What type of membership a member can have
+	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	pub enum Membership {
+		Platinum,
+		Gold,
+		Standard,
+	}
+
+	// implementation for Membership
+	impl Membership {
+		fn get_quota(&self) -> u8 {
+			match *self {
+				Membership::Platinum => 10,
+				Membership::Gold => 5,
+				Membership::Standard => 1,
+			}
+		}
+	}
+
+	/// ### A Member
+	/// Member of the Greeter community\
+	/// stores info\
+	/// 1. greet_count: the number of times user has greeted
+	/// 2. what is type of his membership
+	/// 3. id, to recognize the use with his accountID
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen, Copy)]
+	#[scale_info(skip_type_params(T))]
+	#[codec(mel_bound())]
+	pub struct Member<T: Config> {
+		pub greet_count: u8,
+		pub member_type: Membership,
+		pub id: T::AccountId,
 	}
 
 	#[pallet::event]
