941d750ff68823d29eefa747d7b8d8c1407d306e
Awesome <yourarj@gmail.com>
2022-03-31 22:45:08 +0530

    chore(greeter): added new tests for greeter pallet

--- pallets/greeter/src/tests.rs
+++ pallets/greeter/src/tests.rs
@@ -10,3 +10,22 @@ fn it_works_for_default_value() {
 		assert_noop!(SubsGreeter::greet(Origin::signed(1)), Error::<Test>::QuotaExceeded);
 	});
 }
+
+#[test]
+fn test_alter_membership() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(SubsGreeter::alter_membership(Origin::signed(1), "gold".into()));
+	});
+}
+
+#[test]
+fn test_should_fail_alter_membership() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_noop!(
+			SubsGreeter::alter_membership(Origin::signed(1), "invalid_membership_string".into()),
+			Error::<Test>::InvalidUpgrade
+		);
+	});
+}
